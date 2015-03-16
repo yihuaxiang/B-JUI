774eebe4babe3484613b21c132bb9774f17e6108
fudongdong <fudongdonguestc@gmail.com>
2015-03-16 17:01:44 +0800

    init project

---
+++ BJUI/js/bjui-core.js
@@ -0,0 +1,176 @@
+/*!
+ * B-JUI v1.0 (http://b-jui.com)
+ * Git@OSC (http://git.oschina.net/xknaan/B-JUI)
+ * Copyright 2014 K'naan (xknaan@163.com).
+ * Licensed under Apache (http://www.apache.org/licenses/LICENSE-2.0)
+ */
+
+/* ========================================================================
+ * B-JUI: bjui-core.js v1.0
+ * @author K'naan (xknaan@163.com)
+ * -- Modified from dwz.core.js (author:ZhangHuihua@msn.com)
+ * http://git.oschina.net/xknaan/B-JUI/blob/master/BJUI/js/bjui-core.js
+ * ========================================================================
+ * Copyright 2014 K'naan.
+ * Licensed under Apache (http://www.apache.org/licenses/LICENSE-2.0)
+ * ======================================================================== */
+
++function ($) {
+    'use strict';
+    
+    var BJUI = {
+        JSPATH     : 'BJUI/',
+        PLUGINPATH : 'BJUI/plugins/',
+        IS_DEBUG   : false,
+        keyCode: {
+            ENTER : 13, ESC  : 27, END: 35, HOME: 36,
+            SHIFT : 16, TAB  : 9,
+            LEFT  : 37, RIGHT: 39, UP : 38, DOWN: 40,
+            DELETE: 46, BACKSPACE: 8
+        },
+        eventType: {
+            initUI         : 'bjui.initUI',         // When document load completed or ajax load completed, B-JUI && Plugins init 
+            beforeInitUI   : 'bjui.beforeInitUI',   // If your DOM do not init [add to DOM attribute 'data-noinit="true"']
+            afterInitUI    : 'bjui.afterInitUI',    // 
+            ajaxStatus     : 'bjui.ajaxStatus',     // When performing ajax request, display or hidden progress bar
+            resizeGrid     : 'bjui.resizeGrid',     // When the window or dialog resize completed
+            beforeAjaxLoad : 'bjui.beforeAjaxLoad', // When perform '$.fn.ajaxUrl', to do something...
+            
+            beforeLoadNavtab  : 'bjui.beforeLoadNavtab',
+            beforeLoadDialog  : 'bjui.beforeLoadDialog',
+            afterLoadNavtab   : 'bjui.afterLoadNavtab',
+            afterLoadDialog   : 'bjui.afterLoadDialog',
+            beforeCloseNavtab : 'bjui.beforeCloseNavtab',
+            beforeCloseDialog : 'bjui.beforeCloseDialog',
+            afterCloseNavtab  : 'bjui.afterCloseNavtab',
+            afterCloseDialog  : 'bjui.afterCloseDialog'
+        },
+        pageInfo: {pageCurrent:'pageCurrent', pageSize:'pageSize', orderField:'orderField', orderDirection:'orderDirection'},
+        ajaxTimeout: 30000,
+        alertTimeout: 6000, //alertmsg close timeout
+        statusCode: {ok:200, error:300, timeout:301},
+        keys: {statusCode:'statusCode', message:'message'},
+        ui: {
+            showSlidebar     : true,      // After the B-JUI initialization, display slidebar
+            clientPaging     : true,      // Response paging and sorting information on the client
+            overwriteHomeTab : false      // When open an undefined id of navtab, whether overwrite the home navtab
+        },
+        debug: function(msg) {
+            if (this.IS_DEBUG) {
+                if (typeof(console) != 'undefined') console.log(msg)
+                else alert(msg)
+            }
+        },
+        loginInfo: {
+            url    : 'login.html',
+            title  : 'Login',
+            width  : 420,
+            height : 260,
+            mask   : true
+        },
+        loadLogin: function() {
+            var login = this.loginInfo
+            
+            $('body').dialog({id:'bjui-login', url:login.url, title:login.title, width:login.width, height:login.height, mask:login.mask})
+        },
+        init: function(options) {
+            var op = $.extend({}, options)
+            
+            $.extend(BJUI.statusCode, op.statusCode)
+            $.extend(BJUI.pageInfo, op.pageInfo)
+            $.extend(BJUI.loginInfo, op.loginInfo)
+            $.extend(BJUI.ui, op.ui)
+            
+            if (op.JSPATH) this.JSPATH = op.JSPATH
+            if (op.PLUGINPATH) this.PLUGINPATH = op.PLUGINPATH
+            if (op.ajaxTimeout) this.ajaxTimeout = op.ajaxTimeout
+            if (op.alertTimeout) this.alertTimeout = op.alertTimeout
+            
+            this.IS_DEBUG = op.debug || false
+            this.initEnv()
+            
+            if ((!$.cookie || !$.cookie('bjui_theme')) && op.theme) $(this).theme('setTheme', op.theme)
+        },
+        initEnv: function() {
+            $(window).resize(function() {
+                BJUI.initLayout()
+                setTimeout(function() {$(this).trigger(BJUI.eventType.resizeGrid)}, 30)
+            })
+
+            setTimeout(function() {
+                BJUI.initLayout()
+                $(document).initui()
+            }, 10)
+        },
+        initLayout: function() {
+            var iContentW = $(window).width() - (BJUI.ui.showSlidebar ? $('#bjui-sidebar').width() + 10 : 8) - 0
+            var iContentH = $(window).height() - $('#bjui-header').height() - $('#bjui-hnav').outerHeight() - $('#bjui-footer').outerHeight()
+            var navtabH   = $('#bjui-navtab').find('.tabsPageHeader').height()
+            var topH      = $('#bjui-hnav').height() + $('#bjui-header').outerHeight() + 5
+            var collH     = $('#bjui-hnav').find('.navbar-collapse').height()
+            
+            $('#bjui-container').height(iContentH)
+            $('#bjui-navtab').width(iContentW)
+            $('#bjui-leftside, #bjui-sidebar, #bjui-sidebar-s, #bjui-splitBar, #bjui-splitBarProxy').css({height:'100%'})
+            $('#bjui-navtab .tabsPageContent').height(iContentH - navtabH)
+            
+            /* fixed pageFooter */
+            setTimeout(function() {
+                $('#bjui-navtab > .tabsPageContent > .navtabPage').resizePageH()
+                $('#bjui-navtab > .tabsPageContent > .navtabPage').find('.bjui-layout').resizePageH()
+            }, 10)
+            
+            /* header navbar */
+            var navbarWidth = $('body').data('bjui.navbar.width'),
+                $header = $('#bjui-header'), $toggle = $header.find('.bjui-navbar-toggle'), $logo = $header.find('.bjui-navbar-logo'), $navbar = $('#bjui-navbar-collapse'), $nav = $navbar.find('.bjui-navbar-right')
+            
+            if (!navbarWidth) {
+                navbarWidth = {logoW:$logo.outerWidth(), navW:$nav.outerWidth()}
+                $('body').data('bjui.navbar.width', navbarWidth)
+            }
+            if (navbarWidth) {
+                if ($(window).width() - navbarWidth.logoW < navbarWidth.navW) {
+                    $toggle.show()
+                    $navbar.addClass('collapse menu')
+                } else {
+                    $toggle.hide()
+                    $navbar.removeClass('collapse menu in')
+                }
+            }
+            /* horizontal navbar */
+            var hnavWidth = $('body').data('bjui.hnav.width'), hnavCWidth = $('body').data('bjui.hnav.cwidth'),
+                $hnav = $('#bjui-hnav'), $htoggle = $hnav.find('.bjui-hnav-toggle'), $hnavbar = $('#bjui-hnav-navbar'), $form = $hnav.find('.hnav-form')
+            
+            if (!hnavWidth) {
+                hnavWidth = {barW:$hnavbar.outerWidth(), formW:$form.outerWidth()}
+                $('body').data('bjui.hnav.width', hnavWidth)
+            }
+            if (hnavWidth) {
+                if ((!hnavCWidth ? hnavWidth.barW : hnavCWidth) + hnavWidth.formW > $(window).width()) {
+                    if (!hnavCWidth) {
+                        $hnavbar.addClass('condensed')
+                        $('body').data('bjui.hnav.cwidth', $hnavbar.outerWidth())
+                    }
+                    $htoggle.show()
+                    $hnavbar.addClass('collapse menu')
+                    $form.hide()
+                } else {
+                    if (hnavCWidth && (hnavWidth.barW + hnavWidth.formW < $(window).width())) {
+                        $hnavbar.removeClass('condensed')
+                        $('body').removeData('bjui.hnav.cwidth')
+                    }
+                    $hnavbar.removeClass('collapse menu in')
+                    $form.show()
+                    $htoggle.hide()
+                }
+            }
+        },
+        regional: {},
+        setRegional: function(key, value) {
+            BJUI.regional[key] = value
+        }
+    }
+    
+    window.BJUI = BJUI
+    
+}(jQuery);
---
+++ BJUI/js/bjui-slidebar.js
@@ -0,0 +1,266 @@
+/*!
+ * B-JUI v1.0 (http://b-jui.com)
+ * Git@OSC (http://git.oschina.net/xknaan/B-JUI)
+ * Copyright 2014 K'naan (xknaan@163.com).
+ * Licensed under Apache (http://www.apache.org/licenses/LICENSE-2.0)
+ */
+
+/* ========================================================================
+ * B-JUI: bjui-slidebar.js v1.0
+ * @author K'naan (xknaan@163.com)
+ * -- Modified from dwz.barDrag.js (author:Roger Wu)
+ * http://git.oschina.net/xknaan/B-JUI/blob/master/BJUI/js/bjui-slidebar.js
+ * ========================================================================
+ * Copyright 2014 K'naan.
+ * Licensed under Apache (http://www.apache.org/licenses/LICENSE-2.0)
+ * ======================================================================== */
+
++function ($) {
+    'use strict';
+
+    // SLIDEBAR CLASS INSTANCE
+    // ======================
+    $(function() {
+        $('#bjui-leftside').after('<!-- Adjust the width of Left slide -->').after(FRAG.splitBar).after(FRAG.splitBarProxy)
+    })
+    
+    // SLIDEBAR CLASS DEFINITION
+    // ======================
+    
+    var Slidebar = function(element, options) {
+        this.$element   = $(element)
+        this.$bar       = this.$element.find('#bjui-sidebar')
+        this.$sbar      = this.$element.find('#bjui-sidebar-s')
+        this.$lock      = this.$bar.find('> .toggleCollapse > .lock')
+        this.$navtab    = $('#bjui-navtab')
+        this.$collapse  = this.$sbar.find('.collapse')
+        this.$split     = $('#bjui-splitBar')
+        this.$split2    = $('#bjui-splitBarProxy')
+        
+        this.isfloat    = false
+        this.options    = options
+    }
+    
+    Slidebar.prototype.lock = function() {
+        var that   = this
+        var cleft  = that.$bar.outerWidth() + 6
+        var cwidth = that.$navtab.outerWidth() - cleft + 6
+        
+        that.faLock()
+        that.hoverLock()
+        that.$sbar.animate({left: -10}, 20)
+        that.$bar.removeClass('shadown')
+        that.isfloat = false
+        that.$navtab.animate({left:cleft, width:cwidth}, 500, function() {
+            $(window).trigger(BJUI.eventType.resizeGrid)
+        })
+        that.$split.show()
+    }
+    
+    Slidebar.prototype.unlock = function() {
+        var that    = this
+        var barleft = 0 - that.$bar.outerWidth() - 2
+        var cwidth  = (BJUI.ui.showSlidebar ? that.$bar.outerWidth() : 0) + that.$navtab.outerWidth()
+        
+        that.faUnLock()
+        that.hoverUnLock()
+        that.$navtab.animate({left:6, width:cwidth}, 400)
+        that.$bar.animate({left: barleft}, 500, function() {
+            that.$sbar.animate({left:0}, 200)
+            that.$split.hide()
+            $(window).trigger(BJUI.eventType.resizeGrid)
+        })
+        that.isfloat = false
+    }
+    
+    Slidebar.prototype.float = function() {
+        var that  = this
+        
+        that.$sbar.animate({left:-10}, 200)
+        that.$bar.addClass('shadown').animate({left: 2}, 500)
+        that.isfloat = true
+    }
+    
+    Slidebar.prototype.hideFloat = function() {
+        var that    = this
+        var barleft = 0 - that.$bar.outerWidth() - 2
+        
+        that.$bar.animate({left: barleft}, 500, function() {
+            that.$sbar.animate({left:0}, 100)
+        })
+        that.isfloat = false
+    }
+    
+    Slidebar.prototype.hoverLock = function() {
+        var that = this
+        
+        that.$lock
+            .hover(function() {
+                that.tipUnLock()
+                that.faUnLock()
+            }, function() {
+                that.tipLock()
+                that.faLock()
+            })
+    }
+    
+    Slidebar.prototype.hoverUnLock = function() {
+        var that = this
+        
+        that.$lock
+            .hover(function() {
+                that.tipLock()
+                that.faLock()
+            }, function() {
+                that.tipUnLock()
+                that.faUnLock()
+            })
+    }
+    
+    Slidebar.prototype.tipLock = function() {
+        this.$lock.tooltip('destroy').tooltip({ title:'保持锁定，始终显示导航栏', container:'body' })
+    }
+    
+    Slidebar.prototype.tipUnLock = function() {
+        this.$lock.tooltip('destroy').tooltip({ title:'解除锁定，自动隐藏导航栏', container:'body' })
+    }
+    
+    Slidebar.prototype.faLock = function() {
+        this.$lock.find('> i').attr('class', 'fa fa-lock')
+    }
+    
+    Slidebar.prototype.faUnLock = function() {
+        this.$lock.find('> i').attr('class', 'fa fa-unlock-alt')
+    }
+    
+    Slidebar.prototype.init = function() {
+        var that = this
+        
+        if (!BJUI.ui.showSlidebar) {
+            that.unlock()
+        } else {
+            that.hoverLock()
+        } 
+        
+        this.$lock.off('click.bjui.slidebar').on('click.bjui.slidebar', function() {
+            if (that.isfloat) {
+                that.lock()
+            } else {
+                that.unlock()
+            }
+            BJUI.ui.showSlidebar = !BJUI.ui.showSlidebar
+        })
+        
+        this.$collapse.hover(function() {
+            that.float()
+            that.$navtab.click(function() {
+                if (that.isfloat) that.hideFloat()
+            })
+        })
+        
+        this.$split.mousedown(function(e) {
+            that.$split2.each(function() {
+                var $spbar2 = $(this)
+                
+                setTimeout(function() { $spbar2.show() }, 100)
+                $spbar2
+                    .css({visibility:'visible', left: that.$split.css('left')})
+                    .basedrag($.extend(that.options, {obj:that.$bar, move:'horizontal', event:e, stop: function() {
+                        $(this).css('visibility', 'hidden')
+                        var move      = parseInt($(this).css('left')) - parseInt(that.$split.css('left'))
+                        var sbarwidth = that.$bar.outerWidth() + move
+                        var cleft     = parseInt(that.$navtab.css('left')) + move
+                        var cwidth    = that.$navtab.outerWidth() - move
+                        
+                        that.$bar.css('width', sbarwidth)
+                        that.$split.css('left', $(this).css('left'))
+                        that.$navtab.css({left:cleft, width:cwidth})
+                    }}))
+                
+                return false                    
+            })
+        })
+        
+        Slidebar.prototype.initHnav = function() {
+            var that   = this,
+                title  = that.$element.html(),
+                $li    = that.$element.parent(),
+                $trees = $li.find('> ul.ztree'),
+                $box   = $('#bjui-accordionmenu'),
+                $group = $box.find('> .panel:first-child'),
+                $first
+            
+            if ($trees.length) $box.empty()
+            else return
+            
+            $trees.each(function(i) {
+                var $tree      = $(this).clone().attr('id', 'bjui-sidebar-tree'+ i)
+                var $newGroup  = $group.clone()
+                var $panelHead = $newGroup.find('.panel-heading')
+                var $panelBody = $newGroup.find('.panel-collapse').attr('id', 'bjui-accordionmenu-hnav-'+ i).find('> .panel-body').empty()
+                var $paneltit  = $newGroup.find('.panel-heading > h4 > a').attr('href', '#bjui-accordionmenu-hnav-'+ i).html(title)
+                
+                if (i == 0) $first = $paneltit
+                
+                $paneltit.find('> i').attr('class', 'fa fa-caret-square-o-down')
+                $tree.removeAttr('data-noinit').css('display', 'block').appendTo($panelBody)
+                $box.append($newGroup)
+            })
+            
+            $('#bjui-sidebar').initui()
+            $li.addClass('active').siblings().removeClass('active')
+            if ($first && $first.hasClass('collapsed')) $first.trigger('click')
+        }
+        
+    }
+    
+    // SLIDEBAR PLUGIN DEFINITION
+    // =======================
+    
+    function Plugin(option) {
+        var args     = arguments
+        var property = option
+        
+        return this.each(function () {
+            var $this   = $(this)
+            var options = $.extend({}, $this.data(), typeof option == 'object' && option)
+            var data    = $this.data('bjui.slidebar')
+            
+            if (!data) $this.data('bjui.slidebar', (data = new Slidebar(this, options)))
+            
+            if (typeof property == 'string' && $.isFunction(data[property])) {
+                [].shift.apply(args)
+                if (!args) data[property]()
+                else data[property].apply(data, args)
+            } else {
+                data.init()
+            }
+        })
+    }
+
+    var old = $.fn.slidebar
+
+    $.fn.slidebar             = Plugin
+    $.fn.slidebar.Constructor = Slidebar
+    
+    // SLIDEBAR NO CONFLICT
+    // =================
+    
+    $.fn.basedrag.noConflict = function () {
+        $.fn.slidebar = old
+        return this
+    }
+    
+    // SLIDEBAR DATA-API
+    // ==============
+    $(document).one(BJUI.eventType.afterInitUI, function(e) {
+        $('#bjui-leftside').slidebar({minW:150, maxW:700})
+    })
+    
+    $(document).on('click.bjui.slidebar.data-api', '[data-toggle="slidebar"]', function(e) {
+        Plugin.call($(this), 'initHnav')
+        
+        e.preventDefault()
+    })
+    
+}(jQuery);
